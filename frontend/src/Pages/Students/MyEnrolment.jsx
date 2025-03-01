57d78c5568279b139c245eb68a280b0c5b717e55
ashikulislamcse <ashikulislamcse@gmail.com>
2025-03-02 04:56:01 +0600

    Updated

---
+++ frontend/src/Pages/Students/MyEnrolment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyEnrolment = () => {
+  return (
+    <div>
+      <h1>MyEnrolment</h1>
+    </div>
+  )
+}
+
+export default MyEnrolment
---
+++ frontend/src/components/Students/CourseCart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CourseCart = () => {
+  return (
+    <div>
+      <h1>Course Cart</h1>
+    </div>
+  )
+}
+
+export default CourseCart

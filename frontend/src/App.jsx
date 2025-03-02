452fc291a25d999f61e3ad24682b29f966ee6641
ashikulislamcse <ashikulislamcse@gmail.com>
2025-03-03 04:45:43 +0600

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
-
+import { Route, Routes, useMatch } from 'react-router-dom'
+import Home from './Pages/Students/Home.jsx'
+import CourseList from './Pages/Students/CoursesList.jsx'
+import CourseDetails from './Pages/Students/CourseDetails.jsx'
+import MyEnrollment from './Pages/Students/MyEnrollment.jsx'
+import Player from './Pages/Students/Player.jsx'
+import Loading from './Components/Students/Loading.jsx'
+import Educator from './Pages/Educator/Educator.jsx'
+import Deshboard from './Pages/Educator/Deshboard.jsx'
+import AddCourses from './Pages/Educator/AddCourse.jsx'
+import MyCourses from './Pages/Educator/MyCourses.jsx'
+import StudentEnrolled from './Pages/Educator/StudentEnrolled.jsx'
+import Navbar from './Components/Students/Navbar.jsx'
 const App = () => {
+  const isEducatorRoute = useMatch('/educator/*')
   return (
-    <div>
-      App
+    <div className='text-default min-h-screen bg-white'>
+      {!isEducatorRoute && <Navbar/>}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/course-list' element={<CourseList/>}/>
+        <Route path='/course-list/:input' element={<CourseList/>}/>
+        <Route path='/course-details/:id' element={<CourseDetails/>}/>
+        <Route path='/my-enrollment' element={<MyEnrollment/>}/>
+        <Route path='/player/:courseId' element={<Player/>}/>
+        <Route path='/loading/:path' element={<Loading/>}/>
+        <Route path='/educator' element={<Educator/>}>
+          <Route path='educator' element={<Deshboard/>}/>
+          <Route path='add-course' element={<AddCourses/>}/>
+          <Route path='my-courses' element={<MyCourses/>}/>
+          <Route path='student-enrolled' element={<StudentEnrolled/>}/>
+        </Route>
+      </Routes>
     </div>
   )
 }

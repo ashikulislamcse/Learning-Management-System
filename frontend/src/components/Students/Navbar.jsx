a85892845d035e5c077db8a0d192ed6c162bd556
ashikulislamcse <ashikulislamcse@gmail.com>
2025-03-03 13:36:00 +0600

    Updated

--- frontend/src/components/Students/Navbar.jsx
+++ frontend/src/components/Students/Navbar.jsx
@@ -8,8 +8,10 @@ const Navbar = () => {
   const location = useLocation();
   const isCourseListPage = location.pathname.includes('/course-list');
   const [showPopup, setShowPopup] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
 
   const togglePopup = () => setShowPopup(!showPopup);
+  const toggleForm = () => setIsLogin(!isLogin);
 
   return (
     <div
@@ -41,32 +43,33 @@ const Navbar = () => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <div className="p-6 w-110 bg-white text-sm rounded-2xl shadow-lg relative">
+            <div className="p-6 mt-15 w-110 bg-white text-sm rounded-2xl shadow-lg relative">
               <button
                 onClick={togglePopup}
-                className="absolute top-2 right-2 bg-gray-300 hover:bg-red-500 hover:text-white p-2 rounded-md"
+                className="absolute top-2 right-2 bg-gray-300 hover:bg-red-500 hover:text-white p-2 rounded-md cursor-pointer"
               >
                 <FaRegWindowClose size={20} />
               </button>
               <h1 className="text-2xl text-center font-bold mb-4">
                 Welcome To Educator
               </h1>
+              
               <h2 className="text-2xl text-center font-semibold mb-4">
-                Register
+                {isLogin ? 'Login' : 'Register'}
               </h2>
-
-              <div className="mb-4">
-                <label className="block text-gray-700">Name</label>
-                <div className="flex items-center border rounded-lg p-2">
-                  <FaUser className="text-gray-500 mr-2" />
-                  <input
-                    type="text"
-                    className="w-full focus:outline-none"
-                    placeholder="Enter your name"
-                  />
+              {!isLogin && (
+                <div className="mb-4">
+                  <label className="block text-gray-700">Name</label>
+                  <div className="flex items-center border rounded-lg p-2">
+                    <FaUser className="text-gray-500 mr-2" />
+                    <input
+                      type="text"
+                      className="w-full focus:outline-none"
+                      placeholder="Enter your name"
+                    />
+                  </div>
                 </div>
-              </div>
-
+              )}
               <div className="mb-4">
                 <label className="block text-gray-700">Email</label>
                 <div className="flex items-center border rounded-lg p-2">
@@ -91,24 +94,24 @@ const Navbar = () => {
                 </div>
               </div>
 
+
               <div className="flex justify-between mb-4">
                 <label className="flex items-center text-gray-700">
                   <input type="checkbox" className="mr-2" />
                   Remember me
                 </label>
-                <Link to="/forgot-password" className="text-blue-500">
-                  Forgot Password?
-                </Link>
+                <button className="text-blue-500 cursor-pointer">Forgot Password?</button>
               </div>
 
-              <button className="w-full bg-blue-500 text-white p-2 rounded-md mb-2">
-                Sign Up
+              <button className="w-full bg-blue-500 text-white p-2 rounded-md mb-2 cursor-pointer">
+                {isLogin ? 'Login' : 'Sign Up'}
               </button>
+
               <p className="text-center">
-                Already have an account?{' '}
-                <Link to="/" className="text-blue-500">
-                  Login
-                </Link>
+                {isLogin ? 'Don’t have an account?' : 'Already have an account?'}{' '}
+                <button onClick={toggleForm} className="text-blue-500 cursor-pointer">
+                  {isLogin ? 'Sign Up' : 'Login'}
+                </button>
               </p>
             </div>
           </motion.div>

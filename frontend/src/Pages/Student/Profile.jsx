9b493605d0e23ea8350a8e68141cc685fe1d869d
ashikulislamcse <ashikulislamcse@gmail.com>
2025-03-10 22:45:01 +0600

    Updated

--- frontend/src/Pages/Student/Profile.jsx
+++ frontend/src/Pages/Student/Profile.jsx
@@ -1,67 +1,115 @@
-// import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-// import { Button } from "@/components/ui/button";
-// import {
-//   Dialog,
-//   DialogContent,
-//   DialogHeader,
-//   DialogTitle,
-//   DialogTrigger,
-// } from "@radix-ui/react-dialog";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Loader2 } from "lucide-react";
 
-// import React from "react";
+import React from "react";
+import Course from "./Course";
 
-// const Profile = () => {
-//   return (
-//     <div className="max-w-4xl mx-auto px-4 my-24">
-//       <h1 className="font-bold text-2xl text-center md:text-left">PROFILE</h1>
-//       <div className="flex flex-col md:flex-row items-center md:items-start gap-8 my-5">
-//         <div className="flex flex-col items-center">
-//           <Avatar className="h-24 w-24 md:h-32 md:w-32 mb-4">
-//             <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-//             <AvatarFallback>CN</AvatarFallback>
-//           </Avatar>
-//         </div>
-//         <div>
-//           <div>
-//             <h1 className="font-semibold text-gray-900 dark:text-gray-100 ">
-//               Name:
-//               <span className="font-normal text-gray-700 dark:text-gray-300 ml-2">
-//                 Ashikul Islam
-//               </span>
-//             </h1>
-//           </div>
-//           <div className="mb-2">
-//             <h1 className="font-semibold text-gray-900 dark:text-gray-100 ">
-//               Email:
-//               <span className="font-normal text-gray-700 dark:text-gray-300 ml-2">
-//                 ashikulislamcse@gmail.com
-//               </span>
-//             </h1>
-//           </div>
-//           <div className="mb-2">
-//             <h1 className="font-semibold text-gray-900 dark:text-gray-100 ">
-//               Role:
-//               <span className="font-normal text-gray-700 dark:text-gray-300 ml-2">
-//                 Student
-//               </span>
-//             </h1>
-//           </div>
-//           <Dialog>
-//             <DialogTrigger asChild>
-//               <Button size="sm" className="mt-2">
-//                 Edit Profile
-//               </Button>
-//             </DialogTrigger>
-//             <DialogContent>
-//               <DialogHeader>
-//                 <DialogTitle>Edit Profile</DialogTitle>
-//               </DialogHeader>
-//             </DialogContent>
-//           </Dialog>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+const Profile = () => {
+  const isLoading = false;
+  const enrolledCourses = [1, 2, 3];
+  return (
+    <div className="max-w-4xl mx-auto px-4 my-24">
+      <h1 className="font-bold text-2xl text-center md:text-left">PROFILE</h1>
+      <div className="flex flex-col md:flex-row items-center md:items-start gap-8 my-5">
+        <div className="flex flex-col items-center">
+          <Avatar className="h-24 w-24 md:h-32 md:w-32 mb-4">
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
+        <div>
+          <div>
+            <h1 className="font-semibold text-gray-900 dark:text-gray-100 ">
+              Name:
+              <span className="font-normal text-gray-700 dark:text-gray-300 ml-2">
+                Ashikul Islam
+              </span>
+            </h1>
+          </div>
+          <div className="mb-2">
+            <h1 className="font-semibold text-gray-900 dark:text-gray-100 ">
+              Email:
+              <span className="font-normal text-gray-700 dark:text-gray-300 ml-2">
+                ashikulislamcse@gmail.com
+              </span>
+            </h1>
+          </div>
+          <div className="mb-2">
+            <h1 className="font-semibold text-gray-900 dark:text-gray-100 ">
+              Role:
+              <span className="font-normal text-gray-700 dark:text-gray-300 ml-2">
+                Student
+              </span>
+            </h1>
+          </div>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline">Edit Profile</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Edit profile</DialogTitle>
+                <DialogDescription>
+                  Make changes to your profile here. Click save when you're
+                  done.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label>Name</Label>
+                  <Input
+                    type="text"
+                    placeholder="Enter your name"
+                    className="col-span-3"
+                  />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label>Profile Photo</Label>
+                  <Input type="file" accept="image/*" className="col-span-3" />
+                </div>
+              </div>
+              <DialogFooter>
+                <Button disabled={isLoading}>
+                  {isLoading ? (
+                    <>
+                      <Loader2 className="animate-spin mr-2" size={16} />
+                      Please Wait...
+                    </>
+                  ) : (
+                    "Save Changes"
+                  )}
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
+      <div>
+        <h1 className="font-medium text-lg">Couses you are enrolled in:</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-5">
+          {enrolledCourses.length === 0 ? (
+            <h1 className="text-gray-500 dark:text-gray-400 text-center">
+              You are not enrolled in any courses yet.
+            </h1>
+          ) : (
+            enrolledCourses.map((course, index) => <Course key={index} />)
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default Profile;
+export default Profile;

e42267ad2365b556a9bb4d7b646ec911adb1fec2
ashikulislamcse <ashikulislamcse@gmail.com>
2025-04-05 10:26:32 +0600

    Updated

--- frontend/src/Pages/CourseList.jsx
+++ frontend/src/Pages/CourseList.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { dummyCourses } from "../assets/assets";
+import CourseCard from "../Common/CourseCard";
 
 const CourseList = () => {
+  const [allCourses, setAllCourses] = useState([]);
+
+  const getAllCourses = () => {
+    setAllCourses(dummyCourses);
+  };
+
+  useEffect(() => {
+    getAllCourses();
+  }, []);
+
   return (
-    <div>
-      Course List
+    <div className="py-16 md:px-40 px-8 text-center">
+      <h1 className="text-3xl font-medium text-gray-800">All Courses</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-4 md:px-0 md:my-16 my-10 gap-4">
+        {allCourses.map((course, index) => (
+          <CourseCard key={index} course={course} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CourseList
-
-// Course List
+export default CourseList;
